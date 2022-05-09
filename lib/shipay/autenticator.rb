3bbf0e561856035cc999a77eee2589f759330237
Guilherme Gazzinelli <guilherme.gazzinelli@gmail.com>
2022-05-09 10:07:26 -0300

    Adiciona Auth

--- lib/shipay/autenticator.rb
+++ lib/shipay/autenticator.rb
@@ -1,8 +1,14 @@
+require 'jwt'
+
 module Shipay
   class Autenticator
     attr_accessor :access_token, :refresh_token
 
     def initializer
+      
+    end
+
+    def header
       
     end
   end
